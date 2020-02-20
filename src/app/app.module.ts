f4df964a69462bb0da4033670cf12c325a7b2e41
Admin <alan.qin@infoflosolutions.com>
2020-02-20 10:18:03 -0500

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,23 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import {GridAllModule} from '@syncfusion/ej2-angular-grids';
+import {TranslateModule} from '@ngx-translate/core';
+import {MatButtonModule} from '@angular/material';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    GridAllModule,
+    MatButtonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
